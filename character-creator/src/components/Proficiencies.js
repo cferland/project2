d971d7e2e6e2f24b53e2b702f36d12605cd8435b
Corey Ferland <ferlandc@gmail.com>
2019-12-14 14:21:14 -0500

    implemented Proficiencies functionality

--- character-creator/src/components/Proficiencies.js
+++ character-creator/src/components/Proficiencies.js
@@ -10,10 +10,46 @@ class Proficiencies extends Component {
     }
   }
 
+  handleCheck = (event) => {
+    if (event.target.defaultChecked === false) {
+      if (this.state.checked < this.state.limit) {
+        this.setState({
+          checked: this.state.checked + 1
+        })
+        event.target.defaultChecked = true;
+      } else {
+        event.preventDefault();
+        alert(
+          `You can only choose ${this.state.limit} proficiencies from this category!`
+        );
+      }
+    } else if (event.target.defaultChecked === true) {
+      if (this.state.checked <= this.state.limit) {
+        this.setState({
+          checked: this.state.checked - 1
+        })
+        event.target.defaultChecked = false;
+      }
+    }
+  }
+
   render() {
     return (
-      <div>
-
+      <div key={this.props.index}>
+        <h5>Choose {this.props.choiceSet.choose}</h5>
+        {this.props.choiceSet.from.map((option, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="checkbox"
+                onClick={(event) => this.handleCheck(event)}
+                name={option.type}
+                value={option.url}
+              />
+              {option.name}
+            </div>
+          )
+        })}
       </div>
     )
   }
