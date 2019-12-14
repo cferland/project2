1dfa2ca6095017098d93c713a7b411dbe873bcf0
Corey Ferland <ferlandc@gmail.com>
2019-12-14 14:05:40 -0500

    created class choice functionality

--- character-creator/src/App.js
+++ character-creator/src/App.js
@@ -13,10 +13,25 @@ class App extends Component {
     }
   }
 
+  async pickClass(e) {
+    let newClass = await axios.get(`https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/classes/${e.target.value}`);
+    this.setState({
+      class: newClass.data
+    })
+    console.log(this.state.class);
+  }
+
   render() {
     return (
       <div className="App">
-      
+        <form>
+          <select onChange={(e) => this.pickClass(e)} defaultValue="Pick a Class">
+            <option disabled>Pick a Class</option>
+            <option value="1">Barbarian</option>
+            <option value="2">Bard</option>
+            <option value="3">Cleric</option>
+          </select>
+        </form>
       </div>
     );
   }
