a9af3e4395bc2e5312de80e9cb986cd5dde285c3
Corey Ferland <ferlandc@gmail.com>
2019-12-16 10:45:35 -0500

    added buttons for each ability score

--- character-creator/src/components/AbilityScores.js
+++ character-creator/src/components/AbilityScores.js
@@ -15,9 +15,9 @@ class AbilityScores extends Component {
         <li ><button onClick={(e) => this.props.abilityButton(e, 'minus', 'str')}>-</button>STR: {this.props.abilities.str}<button onClick={(e) => this.props.abilityButton(e, 'plus', 'str')}>+</button></li>
         <li ><button onClick={(e) => this.props.abilityButton(e, 'minus', 'dex')}>-</button>DEX: {this.props.abilities.dex}<button onClick={(e) => this.props.abilityButton(e, 'plus', 'dex')}>+</button></li>
         <li ><button onClick={(e) => this.props.abilityButton(e, 'minus', 'con')}>-</button>CON: {this.props.abilities.con}<button onClick={(e) => this.props.abilityButton(e, 'plus', 'con')}>+</button></li>
-        <li >INT: {this.props.abilities.int}</li>
-        <li >WIS: {this.props.abilities.wis}</li>
-        <li >CHA: {this.props.abilities.cha}</li>
+        <li ><button onClick={(e) => this.props.abilityButton(e, 'minus', 'int')}>-</button>INT: {this.props.abilities.int}<button onClick={(e) => this.props.abilityButton(e, 'plus', 'int')}>+</button></li>
+        <li ><button onClick={(e) => this.props.abilityButton(e, 'minus', 'wis')}>-</button>WIS: {this.props.abilities.wis}<button onClick={(e) => this.props.abilityButton(e, 'plus', 'wis')}>+</button></li>
+        <li ><button onClick={(e) => this.props.abilityButton(e, 'minus', 'cha')}>-</button>CHA: {this.props.abilities.cha}<button onClick={(e) => this.props.abilityButton(e, 'plus', 'cha')}>+</button></li>
       </ul>
     )
   }
