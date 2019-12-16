da22abce5503e4cab4d4410bba28758267ee883c
Corey Ferland <ferlandc@gmail.com>
2019-12-16 14:07:10 -0500

    added proficiencies to Character Sheet

--- character-creator/src/components/CharacterSheet.js
+++ character-creator/src/components/CharacterSheet.js
@@ -13,6 +13,27 @@ function CharacterSheet(props) {
       <h4>INT: {props.character.abilities.int}</h4>
       <h4>WIS: {props.character.abilities.wis}</h4>
       <h4>CHA: {props.character.abilities.cha}</h4>
+      <ul>
+        <h2>Skills</h2>
+        <li className={props.character.proficiencies.includes('Skill: Athletics') ? "selected" : "unselected"}>Athletics</li>
+        <li className={props.character.proficiencies.includes('Skill: Acrobatics') ? "selected" : "unselected"}>Acrobatics</li>
+        <li className={props.character.proficiencies.includes('Skill: Stealth') ? "selected" : "unselected"}>Stealth</li>
+        <li className={props.character.proficiencies.includes('Skill: Sleight of Hand') ? "selected" : "unselected"}>Sleight of Hand</li>
+        <li className={props.character.proficiencies.includes('Skill: Survival') ? "selected" : "unselected"}>Survival</li>
+        <li className={props.character.proficiencies.includes('Skill: Animal Handling') ? "selected" : "unselected"}>Animal Handling</li>
+        <li className={props.character.proficiencies.includes('Skill: Nature') ? "selected" : "unselected"}>Nature</li>
+        <li className={props.character.proficiencies.includes('Skill: History') ? "selected" : "unselected"}>History</li>
+        <li className={props.character.proficiencies.includes('Skill: Religion') ? "selected" : "unselected"}>Religion</li>
+        <li className={props.character.proficiencies.includes('Skill: Arcana') ? "selected" : "unselected"}>Arcana</li>
+        <li className={props.character.proficiencies.includes('Skill: Medicine') ? "selected" : "unselected"}>Medicine</li>
+        <li className={props.character.proficiencies.includes('Skill: Investigation') ? "selected" : "unselected"}>Investigation</li>
+        <li className={props.character.proficiencies.includes('Skill: Perception') ? "selected" : "unselected"}>Perception</li>
+        <li className={props.character.proficiencies.includes('Skill: Insight') ? "selected" : "unselected"}>Insight</li>
+        <li className={props.character.proficiencies.includes('Skill: Persuasion') ? "selected" : "unselected"}>Persuasion</li>
+        <li className={props.character.proficiencies.includes('Skill: Deception') ? "selected" : "unselected"}>Deception</li>
+        <li className={props.character.proficiencies.includes('Skill: Intimidation') ? "selected" : "unselected"}>Intimidation</li>
+        <li className={props.character.proficiencies.includes('Skill: Performance') ? "selected" : "unselected"}>Performance</li>
+      </ul>
     </div>
   )
 }
