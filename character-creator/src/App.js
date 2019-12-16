3f832459240225d8a499f5ab3af5cee877e4e704
Corey Ferland <ferlandc@gmail.com>
2019-12-16 16:04:59 -0500

    created header component

--- character-creator/src/App.js
+++ character-creator/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './components/Header';
 import Form from './components/Form';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Form />
       </div>
     );
