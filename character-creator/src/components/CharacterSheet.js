9c23bf72b4d62361f58ec0fd8b49a83c8ca72f36
Corey Ferland <ferlandc@gmail.com>
2019-12-17 14:15:11 -0500

    began character sheet styling

--- character-creator/src/components/CharacterSheet.js
+++ character-creator/src/components/CharacterSheet.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CharacterSheet(props) {
   return (
-    <div>
+    <div className="character-sheet">
       <h1>{props.character.name}</h1>
       <h3>Level 1 {props.character.race} {props.character.class}</h3>
       <h4>Hit Points: {props.character.hitDie + props.character.modifiers.con} (1d{props.character.hitDie})</h4>
@@ -14,29 +14,29 @@ function CharacterSheet(props) {
       <h4>INT: {props.character.abilities.int} ({props.character.modifiers.int >= 0 ? "+" : ""}{props.character.modifiers.int})</h4>
       <h4>WIS: {props.character.abilities.wis} ({props.character.modifiers.wis >= 0 ? "+" : ""}{props.character.modifiers.wis})</h4>
       <h4>CHA: {props.character.abilities.cha} ({props.character.modifiers.cha >= 0 ? "+" : ""}{props.character.modifiers.cha})</h4>
-      <ul>
-        <h2>Skills</h2>
-        <li className={props.character.proficiencies.includes('Skill: Athletics') ? "selected" : "unselected"}>Athletics</li>
-        <li className={props.character.proficiencies.includes('Skill: Acrobatics') ? "selected" : "unselected"}>Acrobatics</li>
-        <li className={props.character.proficiencies.includes('Skill: Stealth') ? "selected" : "unselected"}>Stealth</li>
-        <li className={props.character.proficiencies.includes('Skill: Sleight of Hand') ? "selected" : "unselected"}>Sleight of Hand</li>
-        <li className={props.character.proficiencies.includes('Skill: Survival') ? "selected" : "unselected"}>Survival</li>
-        <li className={props.character.proficiencies.includes('Skill: Animal Handling') ? "selected" : "unselected"}>Animal Handling</li>
-        <li className={props.character.proficiencies.includes('Skill: Nature') ? "selected" : "unselected"}>Nature</li>
-        <li className={props.character.proficiencies.includes('Skill: History') ? "selected" : "unselected"}>History</li>
-        <li className={props.character.proficiencies.includes('Skill: Religion') ? "selected" : "unselected"}>Religion</li>
-        <li className={props.character.proficiencies.includes('Skill: Arcana') ? "selected" : "unselected"}>Arcana</li>
-        <li className={props.character.proficiencies.includes('Skill: Medicine') ? "selected" : "unselected"}>Medicine</li>
-        <li className={props.character.proficiencies.includes('Skill: Investigation') ? "selected" : "unselected"}>Investigation</li>
-        <li className={props.character.proficiencies.includes('Skill: Perception') ? "selected" : "unselected"}>Perception</li>
-        <li className={props.character.proficiencies.includes('Skill: Insight') ? "selected" : "unselected"}>Insight</li>
-        <li className={props.character.proficiencies.includes('Skill: Persuasion') ? "selected" : "unselected"}>Persuasion</li>
-        <li className={props.character.proficiencies.includes('Skill: Deception') ? "selected" : "unselected"}>Deception</li>
-        <li className={props.character.proficiencies.includes('Skill: Intimidation') ? "selected" : "unselected"}>Intimidation</li>
-        <li className={props.character.proficiencies.includes('Skill: Performance') ? "selected" : "unselected"}>Performance</li>
+      <h2>Skills</h2>
+      <ul className="skills">
+        <li><div className={props.character.proficiencies.includes('Skill: Athletics') ? "selected" : "unselected"}></div>Athletics</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Acrobatics') ? "selected" : "unselected"}></div>Acrobatics</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Stealth') ? "selected" : "unselected"}></div>Stealth</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Sleight of Hand') ? "selected" : "unselected"}></div>Sleight of Hand</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Survival') ? "selected" : "unselected"}></div>Survival</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Animal Handling') ? "selected" : "unselected"}></div>Animal Handling</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Nature') ? "selected" : "unselected"}></div>Nature</li>
+        <li><div className={props.character.proficiencies.includes('Skill: History') ? "selected" : "unselected"}></div>History</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Religion') ? "selected" : "unselected"}></div>Religion</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Arcana') ? "selected" : "unselected"}></div>Arcana</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Medicine') ? "selected" : "unselected"}></div>Medicine</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Investigation') ? "selected" : "unselected"}></div>Investigation</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Perception') ? "selected" : "unselected"}></div>Perception</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Insight') ? "selected" : "unselected"}></div>Insight</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Persuasion') ? "selected" : "unselected"}></div>Persuasion</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Deception') ? "selected" : "unselected"}></div>Deception</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Intimidation') ? "selected" : "unselected"}></div>Intimidation</li>
+        <li><div className={props.character.proficiencies.includes('Skill: Performance') ? "selected" : "unselected"}></div>Performance</li>
       </ul>
-      <ul>
-        <h2>Other Proficiencies</h2>
+      <h2>Proficiencies</h2>
+      <ul className="skills">
         {props.character.proficiencies.filter((proficiency) => !proficiency.includes('Skill:')).map((proficiency, index) => {
           return (
             <li key={index}>{proficiency}</li>
