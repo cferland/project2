ab7ec3cfa4f9fe6e57f0f934d8688cf976e594b5
Corey Ferland <ferlandc@gmail.com>
2019-12-19 16:45:36 -0500

    styled instructions

--- character-creator/src/components/Alignment.js
+++ character-creator/src/components/Alignment.js
@@ -3,39 +3,39 @@ import React from 'react';
 function Alignment(props) {
   return (
     <div className="alignment">
-      <div>
+      <div title="Lawful Good characters are principled and righteous.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Lawful Good" />
         Lawful Good
       </div>
-      <div>
+      <div title="Neutral Good are generous and sincere.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Neutral Good" />
         Neutral Good
       </div>
-      <div>
+      <div title="Chaotic Good characters are compassionate and rebellious.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Chaotic Good" />
         Chaotic Good
       </div>
-      <div>
+      <div title="Lawful Neutral characters are disciplined and austere.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Lawful Neutral" />
         Lawful Neutral
       </div>
-      <div>
+      <div title="True Neutral characters are tolerant and impartial.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="True Neutral" />
         True Neutral
       </div>
-      <div>
+      <div title="Chaotic Neutral characters are anarchic and irresponsible.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Chaotic Neutral" />
         Chaotic Neutral
       </div>
-      <div>
+      <div title="Lawful Evil characters are manipulative and calculating.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Lawful Evil" />
         Lawful Evil
       </div>
-      <div>
+      <div title="Neutral Evil characters are callous and hostile.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Neutral Evil" />
         Neutral Evil
       </div>
-      <div>
+      <div title="Chaotic Evil characters are tactless and depraved.">
         <input type="radio" onChange={props.handleRadio} name="alignment" value="Chaotic Evil" />
         Chaotic Evil
       </div>
